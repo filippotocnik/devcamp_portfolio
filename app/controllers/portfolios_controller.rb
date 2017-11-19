22b523f477962c1d7cb254d58720d842291f6455
Filip <filip.potocnik@me.com>
2017-11-19 12:47:43 +0100

    installed devise gem

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,7 @@
 class PortfoliosController < ApplicationController
 
   def index
-    # vzame samo izbrane z querijem, seprav ki imajo subtitle RoR
+    # vzame samo izbrane z querijem, seprav tiste, ki imajo subtitle RoR
     # @portfolio_items = Portfolio.where(subtitle: 'Ruby on Rails')
     # @portfolio_items = Portfolio.angular
     # @portfolio_items = Portfolio.ruby_on_rails_portfolio_items
@@ -14,7 +14,8 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body, technologies_attributes: [:name]))
+    @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body,
+                                                                      technologies_attributes: [:name]))
 
     respond_to do |format|
       if @portfolio_item.save
