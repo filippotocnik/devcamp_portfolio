e071d0995179286c653349ad2c57db5ff3b39fba
Filip <filip.potocnik@me.com>
2018-01-03 15:42:00 +0100

    integrated new gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: 'source-greeting')
     end
   end
+
+  def copyright_generator
+    CopyrightToolFilipp::Renderer.copyright('Filip Potocnik', 'All rights reserved')
+  end
 end
