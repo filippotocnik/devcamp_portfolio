7ceb44f8b824b8219da3dcbeb10265519c97a98c
Filip <filip.potocnik@me.com>
2018-02-10 11:24:06 +0100

    updated file upload forms and refactor placeholder funcionality into view

--- app/models/concerns/placeholder.rb
+++
@@ -1,7 +0,0 @@
-module Placeholder
-  extend ActiveSupport::Concern
-  # we put methods in concerns for using the same between models
-  def self.image_generator(height:, width:)
-    "http://via.placeholder.com/#{height}x#{width}"
-  end
-end
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,8 +3,7 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attr| attr['name'].blank? }
 
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   mount_uploader :main_image, PortfolioUploader
   mount_uploader :thumb_image, PortfolioUploader
@@ -19,12 +18,4 @@ class Portfolio < ApplicationRecord
   end
 
   scope :ruby_on_rails_portfolio_items, -> { where(subtitle: 'Ruby on Rails') }
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    # this ||= is like set this up only if main_image is nil, so dont overwrite nothing
-    self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '300', width: '200')
-  end
 end
